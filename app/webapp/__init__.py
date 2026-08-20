1a82d3b472bb1f5f994765de05be0c8d737e22b1
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-08-21 02:22:41 +0300

    Expose Mini App intake routes

--- app/webapp/__init__.py
+++ app/webapp/__init__.py
@@ -1,3 +1,10 @@
-from .api import router as webapp_api_router
+from fastapi import APIRouter
+
+from .api import router as core_router
+from .intake import router as intake_router
+
+webapp_api_router = APIRouter()
+webapp_api_router.include_router(core_router)
+webapp_api_router.include_router(intake_router)
 
 __all__ = ['webapp_api_router']
