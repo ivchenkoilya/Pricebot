80904a1d4328189d84f03a5202fdfcc14f4c8f42
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-08-20 13:45:36 +0300

    Configure Vite for same-origin FastAPI deployment

---
+++ webapp/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/app/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false,
+  },
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:8080',
+      '/assets': 'http://127.0.0.1:8080',
+    },
+  },
+})
