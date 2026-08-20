bc34517a88be121ad4a50eaf6e494fe697d68746
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-08-21 02:23:20 +0300

    Expose Clarify Memory API

--- app/webapp/__init__.py
+++ app/webapp/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .api import router as core_router
 from .intake import router as intake_router
+from .memory import router as memory_router
 
 webapp_api_router = APIRouter()
 webapp_api_router.include_router(core_router)
 webapp_api_router.include_router(intake_router)
+webapp_api_router.include_router(memory_router)
 
 __all__ = ['webapp_api_router']
