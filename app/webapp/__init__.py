3a1f2d8776d739a1f9f32bd90304189efed58d5b
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-08-21 09:24:59 +0300

    Wire Clarify support API

--- app/webapp/__init__.py
+++ app/webapp/__init__.py
@@ -3,10 +3,12 @@ from fastapi import APIRouter
 from .api import router as core_router
 from .intake import router as intake_router
 from .memory import router as memory_router
+from .support import router as support_router
 
 webapp_api_router = APIRouter()
 webapp_api_router.include_router(core_router)
 webapp_api_router.include_router(intake_router)
 webapp_api_router.include_router(memory_router)
+webapp_api_router.include_router(support_router)
 
 __all__ = ['webapp_api_router']
