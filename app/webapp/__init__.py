abcbad9b7b3100993d47068d6906d6094e859ac6
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-08-22 00:29:46 +0300

    Register Clarify source media API

--- app/webapp/__init__.py
+++ app/webapp/__init__.py
@@ -4,6 +4,7 @@ from .api import router as core_router
 from .copilot import router as copilot_router
 from .intake import router as intake_router
 from .memory import router as memory_router
+from .source_media import router as source_media_router
 from .support import router as support_router
 
 webapp_api_router = APIRouter()
@@ -11,6 +12,7 @@ webapp_api_router.include_router(core_router)
 webapp_api_router.include_router(intake_router)
 webapp_api_router.include_router(memory_router)
 webapp_api_router.include_router(copilot_router)
+webapp_api_router.include_router(source_media_router)
 webapp_api_router.include_router(support_router)
 
 __all__ = ['webapp_api_router']
