71a25078df25b52f820bf7c65b1bc9779932f98c
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-08-21 11:04:22 +0300

    Mount Clarify top-left profile shortcut

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -4,15 +4,18 @@ import App from './AppV1'
 import SupportWidget from './SupportWidget'
 import PlansWidget from './PlansWidget'
 import MaterialCleanupWidget from './MaterialCleanupWidget'
+import ProfileShortcutWidget from './ProfileShortcutWidget'
 import './v1.css'
 import './v1-mobile.css'
 import './support.css'
 import './plans.css'
 import './plan-badge.css'
+import './product-upgrade.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ProfileShortcutWidget />
     <PlansWidget />
     <MaterialCleanupWidget />
     <SupportWidget />
