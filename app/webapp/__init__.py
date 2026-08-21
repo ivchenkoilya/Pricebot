a5ed7bf3324b53987ccb877779eb781100590581
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-08-21 11:41:17 +0300

    Wire Clarify copilot API

--- app/webapp/__init__.py
+++ app/webapp/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .api import router as core_router
+from .copilot import router as copilot_router
 from .intake import router as intake_router
 from .memory import router as memory_router
 from .support import router as support_router
@@ -9,6 +10,7 @@ webapp_api_router = APIRouter()
 webapp_api_router.include_router(core_router)
 webapp_api_router.include_router(intake_router)
 webapp_api_router.include_router(memory_router)
+webapp_api_router.include_router(copilot_router)
 webapp_api_router.include_router(support_router)
 
 __all__ = ['webapp_api_router']
