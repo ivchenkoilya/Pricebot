434ba934e3d51a47439ff9aa338cfdce1bf1b8c7
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-08-21 10:18:02 +0300

    Mount Clarify tariffs and Memory cleanup widgets

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -2,13 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './AppV1'
 import SupportWidget from './SupportWidget'
+import PlansWidget from './PlansWidget'
+import MaterialCleanupWidget from './MaterialCleanupWidget'
 import './v1.css'
 import './v1-mobile.css'
 import './support.css'
+import './plans.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <PlansWidget />
+    <MaterialCleanupWidget />
     <SupportWidget />
   </StrictMode>,
 )
