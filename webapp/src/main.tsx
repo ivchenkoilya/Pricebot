58aecf5527fd437de51f211c6de050f4ba33925a
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-08-21 09:25:47 +0300

    Show support button in Clarify Mini App

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './AppV1'
+import SupportWidget from './SupportWidget'
 import './v1.css'
 import './v1-mobile.css'
+import './support.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <SupportWidget />
   </StrictMode>,
 )
