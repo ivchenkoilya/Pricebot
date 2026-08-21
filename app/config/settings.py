d03b61c6885b6f10c4f4776392272b2c68e0136f
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-08-21 08:41:40 +0300

    Bump Clarify to 1.0.0

--- app/config/settings.py
+++ app/config/settings.py
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
 
     # Core
     app_name: str = 'Clarify'
-    version: str = '0.9.1'
+    version: str = '1.0.0'
     bot_token: str = ''
     admin_telegram_id: int | None = None
     test_mode: bool = True
