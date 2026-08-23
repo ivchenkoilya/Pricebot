f406d914548ea63afb407f1ec0cf8487ca02ebb6
ivchenkoilya <ilya.ivchenkov@inbox.ru>
2026-08-23 23:53:47 +0300

    Switch Mini App to unified prelaunch interface

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,28 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppV1'
-import CopilotWidget from './CopilotWidget'
-import SupportWidget from './SupportWidget'
-import PlansWidget from './PlansWidget'
-import MaterialCleanupWidget from './MaterialCleanupWidget'
-import ProfileShortcutWidget from './ProfileShortcutWidget'
-import ReferralProfileWidget from './ReferralProfileWidget'
-import UXFixesWidget from './UXFixesWidget'
-import './v1.css'
-import './v1-mobile.css'
-import './support.css'
-import './plans.css'
-import './plan-badge.css'
-import './product-upgrade.css'
-import './copilot.css'
-import './referral.css'
-import './ux-fixes.css'
+import LaunchApp from './LaunchApp'
+import './launch.css'
 
 const telegramWebApp = window.Telegram?.WebApp
 
-// Mark the page ready before React mounts. On some Telegram Android launches
-// from persistent reply-keyboard WebApp buttons the WebView waits for ready()
-// while React is still booting, leaving only the background visible.
+// Render the product shell immediately; account and material data load in parallel.
 telegramWebApp?.ready?.()
 telegramWebApp?.expand?.()
 telegramWebApp?.setHeaderColor?.('#061126')
@@ -30,13 +13,6 @@ telegramWebApp?.setBackgroundColor?.('#061126')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <ProfileShortcutWidget />
-    <ReferralProfileWidget />
-    <PlansWidget />
-    <MaterialCleanupWidget />
-    <CopilotWidget />
-    <UXFixesWidget />
-    <SupportWidget />
+    <LaunchApp />
   </StrictMode>,
 )
